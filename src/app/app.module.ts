1966635ff987ea5ab1b35e7d528b2205c53eb931
Aldehuelo <vincentime77@live.fr>
2023-12-26 15:06:16 +0100

    page visite three js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { AccueilComponent3Component } from './component/accueil-component3/accue
 import { AccueilComponent4Component } from './component/accueil-component4/accueil-component4.component';
 import { AccueilComponent5Component } from './component/accueil-component5/accueil-component5.component';
 import { AccueilComponentHeaderComponent } from './component/accueil-component-header/accueil-component-header.component';
+import { WebglComponent } from './component/webgl/webgl.component';
 
 
 @NgModule({
@@ -64,6 +65,7 @@ import { AccueilComponentHeaderComponent } from './component/accueil-component-h
     AccueilComponent4Component,
     AccueilComponent5Component,
     AccueilComponentHeaderComponent,
+    WebglComponent,
     
 
   ],
