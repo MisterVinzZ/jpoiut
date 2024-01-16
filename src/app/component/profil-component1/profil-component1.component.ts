5f85c69532a1d96e70389606e8afbe2517d287db
Alexis Laborie <aalx0412@gmaim.com>
2024-01-16 18:21:51 +0100

    alexis 16/01/2024 18h21 deco

--- src/app/component/profil-component1/profil-component1.component.ts
+++ src/app/component/profil-component1/profil-component1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profil-component1',
@@ -6,8 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./profil-component1.component.css']
 })
 export class ProfilComponent1Component {
+  constructor(private router: Router) { }
   logout(): void{ //méthode pour se déconnecter et supprimer la session
     sessionStorage.removeItem('adminSession')
+    this.router.navigate(['/page-accueil']);
     console.log('Utilisateur déconnecté')
+  }
+  redirection(): void{
+    this.router.navigate(['/page-admin']);
   }
 }
